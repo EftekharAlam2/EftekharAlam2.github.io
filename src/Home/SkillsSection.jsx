43d6c42109a135c2fc8f284e62640df72acd6b18
EftekharAlam2 <hinosuvlmare@gmail.com>
2024-10-10 13:28:13 +0600

    skill section updated

--- src/Home/SkillsSection.jsx
+++ src/Home/SkillsSection.jsx
@@ -84,6 +84,22 @@ const SkillsSection = () => {
           />
           <span className="font-bold">JavaScript</span>
         </div>
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "jQuery" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("jQuery")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
+          <img
+            src="https://i.ibb.co.com/MZKHBBg/jQuery.png"
+            alt="jQuery Logo"
+            className="w-12 h-12 mr-4"
+          />
+          <span className="font-bold">jQuery</span>
+        </div>
+
         <div
           className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
             hoveredSkill === "React" ? "hovered" : ""
@@ -98,20 +114,52 @@ const SkillsSection = () => {
           />
           <span className="font-bold">React</span>
         </div>
+        
         <div
           className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
-            hoveredSkill === "Firebase" ? "hovered" : ""
+            hoveredSkill === "Vue.js" ? "hovered" : ""
           }`}
-          onMouseEnter={() => handleSkillHover("Firebase")}
+          onMouseEnter={() => handleSkillHover("Vue.js")}
           onMouseLeave={() => handleSkillHover(null)}
         >
           <img
-            src="https://i.ibb.co/Z6VddKK/firebase.png"
-            alt="Firebase Logo"
+            src="https://i.ibb.co.com/Ld7tvgB/vue.png"
+            alt="Vue.js Logo"
+            className="w-12 h-10 mr-4"
+          />
+          <span className="font-bold">Vue.js</span>
+        </div>
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "Alpine.js" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("Alpine.js")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
+          <img
+            src="https://i.ibb.co.com/7CMT4PL/alpine-js.png"
+            alt="Alpine.js Logo"
             className="w-12 h-12 mr-4"
           />
-          <span className="font-bold">Firebase</span>
+          <span className="font-bold">Alpine.js</span>
         </div>
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "Node.js" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("Node.js")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
+          <img
+            src="https://i.ibb.co.com/tqhHjcx/nodejs.png"
+            alt="Node.js Logo"
+            className="w-12 h-12 mr-4"
+          />
+          <span className="font-bold">Node.js</span>
+        </div>
+
         <div
           className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
             hoveredSkill === "MongoDB" ? "hovered" : ""
@@ -125,6 +173,81 @@ const SkillsSection = () => {
             className="w-12 h-12 mr-4"
           />
           <span className="font-bold">MongoDB</span>
+        </div>
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "MySQL" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("MySQL")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
+          <img
+            src="https://i.ibb.co.com/b14LRLQ/mysql.png"
+            alt="MySQL Logo"
+            className="w-16 h-10 mr-4"
+          />
+          <span className="font-bold">MySQL</span>
+        </div>
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "PHP" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("PHP")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
+          <img
+            src="https://i.ibb.co.com/Lhz3q88/php.png"
+            alt="PHP Logo"
+            className="w-12 h-12 mr-4"
+          />
+          <span className="font-bold">PHP</span>
+        </div>
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "Laravel" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("Laravel")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
+          <img
+            src="https://i.ibb.co.com/zFsyLt6/laravel.png"
+            alt="Laravel Logo"
+            className="w-18 h-12 mr-4"
+          />
+          <span className="font-bold">Laravel</span>
+        </div>
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "RabbitMQ" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("RabbitMQ")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
+          <img
+            src="https://i.ibb.co.com/12GqJ9V/rabbitmq.png"
+            alt="RabbitMQ Logo"
+            className="w-12 h-12 mr-4"
+          />
+          <span className="font-bold">RabbitMQ</span>
+        </div>
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "Firebase" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("Firebase")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
+          <img
+            src="https://i.ibb.co/Z6VddKK/firebase.png"
+            alt="Firebase Logo"
+            className="w-12 h-12 mr-4"
+          />
+          <span className="font-bold">Firebase</span>
         </div>
       </div>
     </div>
