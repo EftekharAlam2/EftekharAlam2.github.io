3807d48de8541a9fd48d4e9982b67a419a67f549
EftekharAlam2 <hinosuvlmare@gmail.com>
2024-11-02 18:22:45 +0600

    PWA set up & website qr added

--- src/Home/Footer.jsx
+++ src/Home/Footer.jsx
@@ -5,6 +5,7 @@ import "./contact.css";
 import { FaFacebook, FaWhatsapp, FaLinkedin, FaGithub, FaInstagram, FaDiscord } from 'react-icons/fa';
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
+import { QRCodeCanvas } from 'qrcode.react';
 
 const Footer = () => {
   return (
@@ -93,6 +94,9 @@ const Footer = () => {
                 rel="noopener noreferrer" className="text-white text-2xl">
                 <FaDiscord />
             </a>
+          </div>
+          <div className='mt-6'>
+            <QRCodeCanvas value="https://eftekharalam2.github.io/" size={100} />
           </div>
         </div>
       </div>
